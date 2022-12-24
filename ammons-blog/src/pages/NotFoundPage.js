3daab95249d51bc33dfe78c4b3bed39a194d1771
niiquash <qua16006@byui.edu>
2022-12-24 13:58:56 -0700

    created routes for different pages, created articles list and article details page.

---
+++ ammons-blog/src/pages/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1>This is the not found page</h1>
+        </div>
+    )
+}
+
+export default NotFoundPage
---
+++ ammons-blog/src/pages/article-content.js
@@ -0,0 +1,22 @@
+const articles = [
+    {
+        name: "learn-react",
+        title: "The Fastest Way to Learn React",
+        content: [
+            `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores excepturi libero autem consequuntur assumenda quasi esse eligendi non. Accusamus in blanditiis reprehenderit eaque temporibus quis officiis corporis amet autem, distinctio doloremque doloribus tempore obcaecati ipsam accusantium id. Architecto soluta tempore dignissimos impedit inventore quidem commodi quos ratione debitis deleniti numquam beatae nam velit nostrum quod ducimus molestias iste nihil officiis totam, dicta sit ipsam blanditiis quisquam!`,
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, ipsum. Impedit, quos explicabo inventore, deleniti perferendis voluptate id optio ut ab consequatur ullam ipsam sit minus voluptatum rem eum culpa esse pariatur doloremque corrupti, facilis fugit quaerat. Quas placeat ut exercitationem deserunt aperiam cum vero quisquam ducimus illum. Non sapiente, optio error, voluptates molestias vero delectus fuga sed distinctio quae ipsum veritatis pariatur nostrum nobis corrupti!`,
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, et aliquid? Beatae tenetur unde id nesciunt possimus distinctio aperiam officiis consectetur laudantium aspernatur alias molestiae magnam error, consequuntur nisi architecto fugit, corrupti libero, at sapiente excepturi ipsum. Eveniet reiciendis voluptatum sit beatae debitis aut explicabo iure, praesentium adipisci, voluptatem dolore!`
+        ]
+    },
+    {
+        name: "learn-javascript",
+        title: "JavaScript is a Super Power",
+        content: [
+            `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores excepturi libero autem consequuntur assumenda quasi esse eligendi non. Accusamus in blanditiis reprehenderit eaque temporibus quis officiis corporis amet autem, distinctio doloremque doloribus tempore obcaecati ipsam accusantium id. Architecto soluta tempore dignissimos impedit inventore quidem commodi quos ratione debitis deleniti numquam beatae nam velit nostrum quod ducimus molestias iste nihil officiis totam, dicta sit ipsam blanditiis quisquam!`,
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, ipsum. Impedit, quos explicabo inventore, deleniti perferendis voluptate id optio ut ab consequatur ullam ipsam sit minus voluptatum rem eum culpa esse pariatur doloremque corrupti, facilis fugit quaerat. Quas placeat ut exercitationem deserunt aperiam cum vero quisquam ducimus illum. Non sapiente, optio error, voluptates molestias vero delectus fuga sed distinctio quae ipsum veritatis pariatur nostrum nobis corrupti!`,
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, et aliquid? Beatae tenetur unde id nesciunt possimus distinctio aperiam officiis consectetur laudantium aspernatur alias molestiae magnam error, consequuntur nisi architecto fugit, corrupti libero, at sapiente excepturi ipsum. Eveniet reiciendis voluptatum sit beatae debitis aut explicabo iure, praesentium adipisci, voluptatem dolore!`
+        ]
+    },
+]
+
+export default articles;
