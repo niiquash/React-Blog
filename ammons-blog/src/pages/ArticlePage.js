2d662f60208b38825957170ccc0f1eec9ae834a5
niiquash <qua16006@byui.edu>
2022-12-24 18:23:52 -0700

    initialized backend: installed express

--- ammons-blog/src/pages/ArticlePage.js
+++ ammons-blog/src/pages/ArticlePage.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 import articles from './article-content';
+import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
     const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId)
+
+    // if (!article) {
+    //     return <NotFoundPage />
+    // }
+
     return (
         <>
-            <h1>{article.title}</h1>
-            {article.content.map((paragraph, index) => (
-                <p key={index}>{paragraph}</p>
-            ))}
+            {article ?
+                <>
+                    <h1>{article.title}</h1>
+                    {article.content.map((paragraph, index) => (
+                        <p key={index}>{paragraph}</p>
+                    ))}
+                </>
+                :
+                <NotFoundPage />
+            }
+
         </>
     )
 }
